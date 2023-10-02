3875f8be0051648b1877217afbdf88a65ea22533
devarahifz <devara.hifzhurrahman22@gmail.com>
2023-10-02 22:59:51 +0700

    modify public url

--- src/configs/public_url.js
+++ src/configs/public_url.js
@@ -1,6 +1,7 @@
 import { Directus } from '@directus/sdk';
 
 export const url =
+window.location.hostname === 'devcamp-bootcamp.netlify.app' ||
 window.location.hostname === 'localhost'
   ? 'http://143.198.90.40:8055'
   : 'http://143.198.90.40:8055';
