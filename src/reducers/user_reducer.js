a23b861e8a45a4ccae2bafe7ce9a9f1444e7aa23
devarahifz <devara.hifzhurrahman22@gmail.com>
2023-10-03 19:48:05 +0700

    Add Verify Email Token

--- src/reducers/user_reducer.js
+++ src/reducers/user_reducer.js
@@ -82,13 +82,13 @@ export const register = createAsyncThunk(
       kelas: data.kelas,
       email: data.email,
       kata_sandi: data.kata_sandi,
-      token: token
+      verify_token: token
     })
 
     return {
       nama_lengkap: data.nama_lengkap,
       email: data.email,
-      token: token,
+      verify_token: token,
       id: res.id
     }
   }
@@ -101,11 +101,11 @@ export const verifyEmail = createAsyncThunk(
       fields: [
         "id",
         "email",
-        "token"
+        "verify_token"
       ],
       filter: {
-        token: {
-          _eq: data.token
+        verify_token: {
+          _eq: data.verify_token
         }
       }
     })
