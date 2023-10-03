aece2c824f622009b636a15e47dccb8c231b9424
devarahifz <devara.hifzhurrahman22@gmail.com>
2023-10-03 17:32:08 +0700

    Modify CMS URL

--- src/configs/public_url.js
+++ src/configs/public_url.js
@@ -3,7 +3,7 @@ import { Directus } from '@directus/sdk';
 export const url =
 window.location.hostname === 'devcamp-bootcamp.netlify.app' ||
 window.location.hostname === 'localhost'
-  ? 'https://devcamp.duckdns.org/'
-  : 'https://devcamp.duckdns.org/';
+  ? 'https://devcamp.duckdns.org'
+  : 'https://devcamp.duckdns.org';
 
 export const directus = new Directus(url);
